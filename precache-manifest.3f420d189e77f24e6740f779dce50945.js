73bf5a96caaad0434e08a5d0e2e24222ddc13ba5
narh <kpododanielnarh@gmail.com>
2020-04-03 19:22:28 +0000

    Updates

---
+++ precache-manifest.3f420d189e77f24e6740f779dce50945.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e831401642ee67122c22903aeab4b3e3",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "9872aaf997b73c6b82d9",
+    "url": "/covid19/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "f83363a0ac8579ed049e",
+    "url": "/covid19/static/css/main.2d96bacc.chunk.css"
+  },
+  {
+    "revision": "9872aaf997b73c6b82d9",
+    "url": "/covid19/static/js/2.f2f98d0b.chunk.js"
+  },
+  {
+    "revision": "f78d89367331f86ae0c553aad7509467",
+    "url": "/covid19/static/js/2.f2f98d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83363a0ac8579ed049e",
+    "url": "/covid19/static/js/main.b8575d54.chunk.js"
+  },
+  {
+    "revision": "f7905e6c51f0df2a7dd5",
+    "url": "/covid19/static/js/runtime-main.667de02d.js"
+  },
+  {
+    "revision": "d1a746a818a459b74bed44bb69837495",
+    "url": "/covid19/static/media/BLOODY.d1a746a8.TTF"
+  },
+  {
+    "revision": "47f711bd1524b9283aa92d3d74db16a8",
+    "url": "/covid19/static/media/BebasNeue-Regular.47f711bd.ttf"
+  },
+  {
+    "revision": "983da11ab07d562a4de892154225181f",
+    "url": "/covid19/static/media/INFECTED.983da11a.ttf"
+  },
+  {
+    "revision": "229554e190348686234dfbd7da08bb90",
+    "url": "/covid19/static/media/blood.229554e1.jpg"
+  },
+  {
+    "revision": "945cf0ed64f33cd807a65d3bb35093e0",
+    "url": "/covid19/static/media/coronavirus.945cf0ed.png"
+  },
+  {
+    "revision": "91986f972615bc00e01725e9dbe318c1",
+    "url": "/covid19/static/media/crimes.91986f97.ttf"
+  }
+]);
